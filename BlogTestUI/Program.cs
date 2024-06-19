249e7cfcea98a5576bb1a74d8a0d38446bf5887c
Sage Vilaga <itshurtsmans@gmail.com>
2024-06-19 14:07:57 +0800

    added GetCurrentUser() and Authenticate() methods to Program.cs

--- BlogTestUI/Program.cs
+++ BlogTestUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BlogDataLibrary.Database;
 using BlogDataLibrary.Data;
 using Microsoft.Extensions.Configuration;
+using BlogDataLibrary.Models;
 
 namespace BlogTestUI
 {
@@ -9,6 +10,38 @@ namespace BlogTestUI
         static void Main(string[] args)
         {
             SqlData db = GetConnection();
+
+            Authenticate(db);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+
+        public static void Authenticate(SqlData db)
+        {
+            UserModel user = GetCurrentUser(db);
+
+            if (user == null)
+            {
+                Console.WriteLine("Invalid credentials");
+            }
+            else
+            {
+                 Console.WriteLine($"Welcome, {user.UserName}");
+            }
+        }
+
+        private static UserModel GetCurrentUser(SqlData db)
+        {
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            UserModel user = db.Authenticate(username, password);
+
+            return user;
         }
 
         static SqlData GetConnection()
