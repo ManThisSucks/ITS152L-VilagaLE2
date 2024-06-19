fc5d68b332f78a71b91add3b2d4bb64496ebc910
Sage Vilaga <itshurtsmans@gmail.com>
2024-06-19 13:52:27 +0800

    added GetConnection() method to Program.cs, made SqlData public (for real this time)

--- BlogTestUI/Program.cs
+++ BlogTestUI/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace BlogTestUI
+﻿using BlogDataLibrary.Database;
+using BlogDataLibrary.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogTestUI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SqlData db = GetConnection();
+        }
+
+        static SqlData GetConnection()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+            ISqlDataAccess dbAccess = new SqlDataAccess(config);
+            SqlData db = new SqlData(dbAccess);
+
+            return db;
         }
     }
 }
