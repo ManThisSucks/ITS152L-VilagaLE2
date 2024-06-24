876b62d98539f096c0f1a3898dcf7c1006a0324e
Sage Vilaga <itshurtsmans@gmail.com>
2024-06-24 14:37:12 +0800

    LE4 - App Feature Add Post # 1-5

--- BlogAPI/Controllers/PostController.cs
+++ BlogAPI/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using BlogDataLibrary.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BlogAPI.Controllers
 {
@@ -34,6 +35,38 @@ namespace BlogAPI.Controllers
             ListPostModel? post = _db.ShowPostDetails(id);
 
             return Ok(post);
+        }
+
+        private int GetCurrentUserId()
+        {
+            ClaimsIdentity? identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                string? id = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+                if (id != null)
+                {
+                    return Convert.ToInt32(id);
+                }
+            }
+            return 0;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("/api/[controller]/add")]
+        public ActionResult AddPost([FromBody] PostForm form)
+        {
+            PostModel post = new PostModel();
+            post.Title = form.Title;
+            post.Body = form.Body;
+            post.DateCreated = DateTime.Now;
+            post.UserId = GetCurrentUserId();
+            _db.AddPost(post);
+
+            return Ok("Post created.");
         }
     }
 }
